47e3d368fafd7ec692c2cf320c7f7834938209b5
ToniEdwards <mstonielt@gmail.com>
2022-02-22 17:29:22 -0600

    Final Commit

--- leaflet-step-1/static/logic.js
+++ leaflet-step-1/static/logic.js
@@ -2,20 +2,20 @@
 var base = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   });
-  
-  //Mapbox
+//Mapbox
 var myMap = L.map("map",{
   center: [39.73, -104.99],
   zoom: 2,
   layers: [base]
   });
-  
+//Build Markers function
 function buildMarkers() { 
     //Data source
     var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
     //Retrieve data
     d3.json(url).then((data) => {
     //   console.log(data);
+    //Radius conditionals
     function getRadius(magnitude) {
         switch(true) {
           case magnitude > 15:
@@ -26,8 +26,9 @@ function buildMarkers() {
             return 20;
           case magnitude >= 0:
             return 10;
-           }
+        }
        };
+    //Color conditionals
     function getColor(depth) {
         switch(true) {
             case depth > 100:
@@ -58,8 +59,27 @@ function buildMarkers() {
           layer.bindPopup("Magnitude: "+ feature.properties.mag+ "<br>Depth: "+ feature.geometry.coordinates[2]+ "<br>Location: "+ feature.properties.place);
         }
       }).addTo(myMap);
-    
     });
-    
+    var legend = L.control({
+      position: "topright"
+    });
+    legend.onAdd = function() {
+      var div = L.DomUtil.create("div", "earthquakes legend");
+      var grades = [0, 10, 50, 100];
+      var colors = [
+        "#ffffb2",
+        "#fecc5c",
+        "#fd8d3c",
+        "#bd0026"
+      ];
+      div.innerHTML = "<h4>Depth (km) of EarthQuake</h4>"
+      for (var i = 0; i < grades.length; i++) {
+        div.innerHTML += "<row style='background-color:" + colors[i] + "'/>"
+        + grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+      }
+      return div;
+    };
+    legend.addTo(myMap);
 };
+//Initiate buildMarkers function
 buildMarkers();
